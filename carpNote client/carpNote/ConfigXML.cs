cfcce3837142463abe41c4d4a748bb33cf8916a0
liyu <liyubelief@iCloud.com>
2015-03-26 22:30:27 +0800

    client

---
+++ carpNote client/carpNote/ConfigXML.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+
+namespace carpNote
+{
+    class ConfigXML
+    {
+        XmlNode root = null;//根节点
+        public ConfigXML()
+        {
+
+            XmlDataDocument configXMLDocu = new XmlDataDocument();
+            configXMLDocu.Load("./config.xml");
+            root = configXMLDocu.DocumentElement;
+        }
+
+        //得到用户账号
+        public string getUserID()
+        {    
+            XmlNode UserIDNode = root.SelectSingleNode("UserID");
+            return UserIDNode.InnerText;
+        }
+
+        //修改用户账户
+        public bool setUserID(string UserID)
+        {
+            XmlNode UserIDNode = root.SelectSingleNode("UserID");
+            UserIDNode.InnerText = UserID;
+
+            return true;
+        }
+
+        //得到用户密码
+        public string getUserPW()
+        {
+            XmlNode UserPWNode = root.SelectSingleNode("UserPW");
+            return UserPWNode.InnerText;
+        }
+
+        //修改用户密码
+        public bool setUserPW(string UserPW)
+        {
+            XmlNode UserPWNode = root.SelectSingleNode("UserPW");
+            UserPWNode.InnerText = UserPW;
+
+            return true;
+        }
+
+        //得到最后看的书
+        public string getLastUseBook()
+        {
+            XmlNode lastUseBook = root.SelectSingleNode("lastUseBook");
+            return lastUseBook.InnerText;
+        }
+
+        //设置最后看的书
+        public bool SetLastUseBook(string UseBook)
+        {
+            XmlNode lastUseBook = root.SelectSingleNode("lastUseBook");
+            lastUseBook.InnerText = UseBook;
+
+            return true;
+        }
+
+        public string getLastUseNote()
+        {
+            XmlNode lastUseNote = root.SelectSingleNode("lastUseNote");
+            return lastUseNote.InnerText;
+        }
+
+        public bool SetLastUseNote(string UseNote)
+        {
+            XmlNode lastUseNote = root.SelectSingleNode("lastUseNote");
+            lastUseNote.InnerText = UseNote;
+
+            return true;
+        }
+
+    }
+}
---
+++ carpNote client/carpNote/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace carpNote
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MouseLeftButtonDown += (o, args) =>
+            {
+                var hwnd = new WindowInteropHelper(this).Handle;
+                ReleaseCapture();
+                SendMessage(hwnd, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            };
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void but_Exit_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        private void but_Login_Click(object sender, RoutedEventArgs e)
+        {
+            Login loginPage = new Login();
+            loginPage.ShowDialog();
+        }
+
+        private void but_Setting_Click(object sender, RoutedEventArgs e)
+        {
+            Setting setPage = new Setting();
+            setPage.ShowDialog();
+        }
+
+        private void but_syn_Click(object sender, RoutedEventArgs e)
+        {
+            ConfigXML configXML = new ConfigXML();
+            configXML.getUserID();
+        }
+
+
+    }
+}
